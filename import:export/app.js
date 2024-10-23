9fd103753ac2bde568b51859f10bb8edaf9f0820
Ash <redcoolike@gmail.com>
2024-10-23 17:10:07 +0200

    initialCommit

---
+++ import:export/app.js
@@ -0,0 +1,4 @@
+import './game.js'
+
+const game = new Game()
+game.start();
---
+++ import:export/game.js
@@ -0,0 +1,19 @@
+import './domain.js'
+
+export default class Game {
+    start(){
+        console.log('game started')
+    }
+}
+
+class GameSavingData {
+
+}
+
+export function readGameSaving() {
+
+}
+
+ export function writeGameSaving() {
+
+}
